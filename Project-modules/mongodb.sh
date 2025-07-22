56c0a6d1f8db396a8a99ecdd5ed76de36ac890f2
rajesh <mrajesh0525@gmail.com>
2025-07-23 00:32:41 +0530

    roboshop

--- Project-modules/mongodb.sh
+++ Project-modules/mongodb.sh
@@ -33,14 +33,14 @@ validate() {
 cp mongodb.repo /etc/yum.repos.d/mongodb.repo
 VALIDATE $? "Copying mongodb.repo file"
 
-dnf install mongodb-org -y 
+dnf install mongodb-org -y &&>>$logfile
 VALIDATE $? "Installing mongodb-org package"
-systemctl enable mongod
+systemctl enable mongod &&>>$logfile
 VALIDATE $? "Enabling mongod service"
-systemctl start mongod
+systemctl start mongod &&>>$logfile
 VALIDATE $? "Starting mongod service"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Updating bindIp in mongod.conf file"
-systemctl restart mongod
+systemctl restart mongod &&>>$logfile
 VALIDATE $? "Restarting mongod service"

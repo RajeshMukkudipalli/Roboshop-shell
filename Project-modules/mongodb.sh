858b2de2b5324d653d1a925e60ace10e094ce03c
rajesh <mrajesh0525@gmail.com>
2025-07-23 00:10:23 +0530

    roboshop

---
+++ Project-modules/mongodb.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+userid=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+Logs="/var/log/roboshop-logs"
+script_name=$(echo $0 | cut -d   '.' -f1)
+logfile="$Logs/$script_name-$(date +%F).log"
+
+mkdir -p $Logs
+echo "Script started at: $(date)" &>> $logfile
+
+
+
+if [ $userid -ne 0 ]; then
+    echo "You are not root user"
+else
+    echo "You are  root user"
+fi
+# this is a validation function
+validate() {
+    if [ $1 -eq 0 ]; then
+        echo -e "$G Installing $2 is successful" | tee -a $logfile
+    else
+        echo -e "$R Installing $2 is failed"
+        exit 1
+    fi
+}
+
+
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo

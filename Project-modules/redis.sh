41e4a7e7c2d3327c67eb4a8950d10be1360572de
rajesh <mrajesh0525@gmail.com>
2025-08-30 22:55:03 +0530

    shell-roboshop

--- Project-modules/redis.sh
+++ Project-modules/redis.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+Start_time=$(date +%s)
 userid=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -28,4 +30,24 @@ validate() {
         exit 1
     fi
 }
+
+dnf module disable redis -y &&>>$logfile
+validate $? "Disabling redis version"
+
+dnf module enable redis:7 -y &&>>$logfile
+validate $? "Enabling redis version:7"
+
+dnf install redis -y &&>>$logfile
+validate $? "Installing redis package"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protect-mode/ c protect-mode no' /etc/redis/redis.conf
+validate $? "Updating redis.conf file to our desired configuration so that it can communicate between our modules"
+
+systemctl enable redis &&>>$logfile
+validate $? "Enabling redis service"
+systemctl start redis &&>>$logfile
+validate $? "Starting redis service"
+
+
+
 

a33f3c0537c0a93a87b8b73bcc94bcbed3f3a81d
rajesh <mrajesh0525@gmail.com>
2025-09-01 00:06:45 +0530

    shell-roboshop

--- Project-modules/mongodb.sh
+++ Project-modules/mongodb.sh
@@ -1,9 +1,34 @@
 #! /bin/bash
 
-source ./common.sh
-app_name=mongodb
+userid=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+Logs="/var/log/roboshop-logs"
+script_name=$(echo $0 | cut -d   '.' -f1)
+logfile="$Logs/$script_name-$(date +%F).log"
 
-check_root_user
+mkdir -p $Logs
+echo "Script started at: $(date)" &>> $logfile
+
+
+
+if [ $userid -ne 0 ]; then
+    echo "You are not root user"
+else
+    echo "You are  root user"
+fi
+# this is a validation function
+validate() {
+    if [ $1 -eq 0 ]; then
+        echo -e "$G $2 is successful" | tee -a $logfile
+    else
+        echo -e "$R  $2 is failed"
+        exit 1
+    fi
+}
+
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Copying mongodb.repo file"
@@ -19,7 +44,6 @@ VALIDATE $? "Starting mongod service"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Updating bindIp in mongod.conf file"
+
 systemctl restart mongod &&>>$logfile
 VALIDATE $? "Restarting mongod service"
-
-print_time

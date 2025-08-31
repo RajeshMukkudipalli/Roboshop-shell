292ec5d2adbd8d6ec896b37f3bca2120a234eea2
rajesh <mrajesh0525@gmail.com>
2025-09-01 00:35:25 +0530

    shell-roboshop

--- Optimized-modules/rabbitmq.sh
+++ Optimized-modules/rabbitmq.sh
@@ -1,38 +1,13 @@
 #! /bin/bash
 
 
-Start_time=$(date +%s)
-userid=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-Logs="/var/log/roboshop-logs"
-script_name=$(echo $0 | cut -d   '.' -f1)
-logfile="$Logs/$script_name-$(date +%F).log"
-script_dir=$(pwd)
-
-mkdir -p $Logs
-echo "Script started at: $(date)" &>> $logfile
-
-
-
-if [ $userid -ne 0 ]; then
-    echo "You are not root user"
-else
-    echo "You are  root user"
-fi
+sosurce ./common.sh
+app_name=rabbitmq
+check_root_user
 echo "please enter mysql root password"
-read -s RABBITMQ_PASSWORD
+read -s RABBITMQ_PASSWORD  #roboshop123
 # this is a validation function
-validate() {
-    if [ $1 -eq 0 ]; then
-        echo -e "$G $2 is successful" | tee -a $logfile
-    else
-        echo -e "$R  $2 is failed"
-        exit 1
-    fi
-}
+
 
 cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
 validate $? "Copying rabbitmq.repo file"
@@ -49,8 +24,4 @@ validate $? "Starting rabbitmq-server service"
 rabbitmqctl add_user roboshop $RABBITMQ_PASSWORD
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
 
-
-
-End_time=$(date +%s)
-Total_time=$(($End_time - $Start_time))
-echo -e "$Y Total time took to execute the script: $Total_time seconds $N" | tee -a $logfile
+print_time

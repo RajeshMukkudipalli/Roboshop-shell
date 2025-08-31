33194b8d218ced76a5245b101bdf1663c47bf170
rajesh <mrajesh0525@gmail.com>
2025-08-31 21:31:43 +0530

    shell-roboshop

---
+++ Project-modules/script-1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+greet="Hello Good Morning"
+echo "$greet"
+echo "process id of script-1 is pid-1 $$"
+
+#./script-2.sh
+
+source ./script-2.sh
---
+++ Project-modules/script-2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gr="Hello Good Morning"
+echo "$gr"
+echo "process id of script-w is pid-1 $$"

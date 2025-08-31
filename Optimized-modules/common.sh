f8905ce57f6042d9e45807c935102b9f89365cd8
rajesh <mrajesh0525@gmail.com>
2025-08-31 22:42:06 +0530

    shell-roboshop

--- Optimized-modules/common.sh
+++ Optimized-modules/common.sh
@@ -15,6 +15,55 @@ script_dir=$(pwd)
 mkdir -p $Logs
 echo "Script started at: $(date)" &>> $logfile
 
+app_setup(){
+    id roboshopp
+    if [ $? -ne 0 ]
+    then
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+        validate $? "Creating roboshop user"
+    else
+        echo -e "roboshop user already exists"
+    fi
+
+    mkdir  /app
+    validate $? "Creating /app directory"
+
+    curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+
+    rm -rf /app/* &&>>$logfile
+    cd /app
+
+    unzip /tmp/catalogue.zip
+    validate $? "Unzipping catalogue.zip file"
+}
+
+node_js_setup(){
+    dnf module disable nodejs -y &&>>$logfile
+    validate $? "Disabling nodejs package"
+
+    dnf module enable nodejs:20 -y &&>>$logfile
+    validate $? "Enabling nodejs package:20"
+
+    dnf install nodejs -y &&>>$logfile
+    validate $? "Installing nodejs package" 
+    
+    npm install &&>>$logfile
+    validate $? "Installing npm packages"
+}
+
+systemd_setup(){
+    cp $script_dir/catalogue.service /etc/systemd/system/catalogue.service
+    validate $? "Copying catalogue.service file"
+
+    systemctl daemon-reload &&>>$logfile
+    validate $? "Reloading systemd daemon"
+
+    systemctl enable catalogue &&>>$logfile
+    validate $? "Enabling catalogue service"   
+
+    systemctl start catalogue &&>>$logfile
+    validate $? "Starting catalogue service"
+}
 
 check_root_user(){
     if [ $userid -ne 0 ]; then

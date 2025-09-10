161c198c6accfdac91163c3f3bbc7ff7cf680d15
rajesh <mrajesh0525@gmail.com>
2025-09-11 01:00:34 +0530

    shell-roboshop

---
+++ robo.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+ami_id=ami-09c813fb71547fc4f
+sg_id=sg-0b267a41ee6ac45fc
+INSTANCES=("catalogue" "cart" "dispatch" "frontend" "mongodb" "mysql" "payment" "rabbitmq" "shipping" "redis" "user")
+zone_id=Z078325528QCYPHSKGLAG
+DomainName=devopsmaster.xyz
+
+for instance in ${INSTANCES[@]}; do
+    INSTANCE_ID=$(aws ec2 run-instances \
+        --image-id $ami_id \
+        --instance-type t2.micro \
+        --security-group-ids $sg_id \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" \
+        --query "Instances[0].InstanceId" \
+        --output text)
+
+    if [ "$instance" != "frontend" ]; then
+        IP=$(aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query "Reservations[0].Instances[0].PrivateIpAddress" \
+            --output text)
+        Record_name="$instance.$DomainName"
+    else
+        IP=$(aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query "Reservations[0].Instances[0].PublicIpAddress" \
+            --output text)
+        Record_name="$DomainName"
+    fi
+
+    echo "$instance IP address: $IP"
+
+    aws route53 change-resource-record-sets \
+      --hosted-zone-id $zone_id \
+      --change-batch '{
+        "Changes": [
+          {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+              "Name": "'"$Record_name"'",
+              "Type": "A",
+              "TTL": 60,
+              "ResourceRecords": [
+                {
+                  "Value": "'"$IP"'"
+                }
+              ]
+            }
+          }
+        ]
+      }'
+done
